6bdc44daa4d8ae2eaf16ffa14a78fa109ccd3f29
FAST <krinickij.v.a@gmail.com>
2024-10-30 19:15:43 +0300

    fix: trash icon color

--- src/components/ui/buttons/Trash.tsx
+++ src/components/ui/buttons/Trash.tsx
@@ -33,7 +33,7 @@ export const Trash: FC<ButtonProps> = () => {
         <>
 
             <button onClick={openConfirmPopup} className="bg-red-400 h-10 w-10 rounded-lg flex items-center justify-center group hover:bg-red-600">
-                <svg className="w-8 h-8 text-gray-800 dark:text-white fill-slate-400  group-hover:fill-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <svg className="w-8 h-8 text-gray-800 dark:text-white fill-white  group-hover:fill-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path fillRule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clipRule="evenodd" />
                 </svg>
             </button>
