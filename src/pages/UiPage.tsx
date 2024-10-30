71c134a540ee2b4d9c325842c23f6ab828318322
FAST <krinickij.v.a@gmail.com>
2024-10-30 10:28:51 +0300

    feat: edited loader wrapper + aded loader for UI page

--- src/pages/UiPage.tsx
+++ src/pages/UiPage.tsx
@@ -1,9 +1,19 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ButtonType1 } from "../components/ui/buttons/SybmitButton";
 import { Trash } from "../components/ui/buttons/Trash";
 import { Edit } from "../components/ui/buttons/Edit";
+import { Loader } from "../components/general/Loader";
 
 export const UiPage: FC = () => {
+    const [loaderState, setLoaderState]=useState(false)
+
+    const showLoader=()=>{
+        setLoaderState(true)
+        setTimeout(()=>{
+            setLoaderState(false)
+        },3000)
+
+    }
     return (
         <>
             <h1 className=" text-center font-bold text-3xl mb-10">Страница для ui компонентов</h1>
@@ -21,9 +31,9 @@ export const UiPage: FC = () => {
                         <div className="">
                             Параметры:
                             <ul>
+                                <li>name : string</li>
                                 <li>type : bytton | submit</li>
                                 <li>onclick? : function</li>
-
                             </ul>
                         </div>
                     </div>
@@ -61,6 +71,35 @@ export const UiPage: FC = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="">
+                <h2 className="text-center ">Обёртки</h2>
+                <div className="flex gap-7">
+                    <div className="w-72">
+                    <Loader
+                            loadingState={loaderState}/>
+                        <ButtonType1
+                            name="вызов Лоадера"
+                            onClick={showLoader}
+                        />
+
+                           
+                        <div className="flex flex-row gap-2">
+                            <span className="font-bold">имя компонента:</span>
+                            <span className="font-bold text-green-900">Loader</span>
+                        </div>
+                        <div className="">
+                            Параметры:
+                            <ul>
+                                <li>loadingState : boolean</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
 
 
 
