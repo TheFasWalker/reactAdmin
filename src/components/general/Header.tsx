ce4fd514ac306dc2e27b8a8272a0a10591afbce8
FAST <krinickij.v.a@gmail.com>
2024-10-31 08:18:30 +0300

    fix: fix header styles

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <nav className="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
       <div className="flex flex-wrap justify-between items-center">
-        <div className="flex justify-start items-center">
+        <div className="flex justify-between items-center w-full">
 
           <a href="https://flowbite.com" className="flex items-center justify-between mr-4">
             <img
@@ -14,10 +14,13 @@ export const Header = () => {
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
           </a>
+          <div className="">
           <WhiteButton
             title={"LogOut"}
             icon='exit'
           />
+          </div>
+          
 
         </div>
 
