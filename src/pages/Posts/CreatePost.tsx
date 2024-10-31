81fb5ad4b284452b0f5d1a4d665dc76957098aae
FAST <krinickij.v.a@gmail.com>
2024-10-31 18:16:11 +0300

    feat: edited tinymce to project
    добавлена библиотека tinymce
    Добавлена инициалазация оной на странице создания поста

--- src/pages/Posts/CreatePost.tsx
+++ src/pages/Posts/CreatePost.tsx
@@ -1,12 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { CreateEditHeader } from "../../components/general/CreateEditHeader";
-
+import { Editor } from '@tinymce/tinymce-react';
 
 export const CreatePost:FC=()=>{
+    const [content, setContent] = useState('');
+
+    const handleEditorChange = (content, editor) => {
+        setContent(content);
+    };
     return(
         <div className="">
             <CreateEditHeader
             title="Создание поста"/>
+
+
+
+
+        <Editor
+            apiKey='a3dmilatwmfsmji2q2818ite1q0zkdkvn9bb1zd2thldryh9'
+                initialValue="<p>Начните писать здесь...</p>"
+                textareaName="description"
+                init={{
+                    height: 500,
+                    menubar: false,
+
+                }}
+                onEditorChange={handleEditorChange}
+            />
         </div>
     )
 }
