3de1ebc7a6a425797f9fc78e231b3b224effaa91
FAST <krinickij.v.a@gmail.com>
2024-11-05 14:26:37 +0300

    FEAT : Edited creating User form
    в проект добавлен yup
    в попап создания пользователя добавлен формик
    Написана валидация для  поля name
    Обновлен компонент InputField в него добавлены недостающие для рабоыт с формик данные

--- src/components/ui/form/InputField.tsx
+++ src/components/ui/form/InputField.tsx
@@ -3,13 +3,32 @@ interface InputFieldInterface{
     title:string,
     name:string,
     placeholder:string,
-    type:'text' | 'email'
+    type:'text' | 'email',
+    onchange?:(e: React.ChangeEvent<HTMLInputElement>)=>void,
+    onblure?:(e: React.FocusEvent<HTMLInputElement>)=>void,
+    value?:string,
+    touched?:boolean,
+    error?:string
 }
-export const InputField:FC<InputFieldInterface>=({title,name,placeholder,type})=>{
+export const InputField:FC<InputFieldInterface>=({title,name,placeholder,type,onchange,onblure,value='',touched,error})=>{
     return(
         <label>
-        <span>{title} </span>
-        <input type={type} value='' placeholder={placeholder} name={name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <span className={`${touched && error && ('flex w-full justify-between')}`}>
+            <span>{title} </span>
+            {touched && error && <p className="text-red-500">{error}</p>}
+            </span>
+
+        <input
+            onChange={onchange} 
+            onBlur={onblure}
+            type={type} 
+            value={value} 
+            placeholder={placeholder} 
+            name={name} 
+            className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+                ${touched && error &&('border-red-500 border-4')}`} 
+        />
+        
     </label>
     )
 }
--- src/components/users/UserCreatePopup.tsx
+++ src/components/users/UserCreatePopup.tsx
@@ -5,18 +5,59 @@ import { ButtonType1 } from "../ui/buttons/SybmitButton";
 import { PopUpWrapper } from "../general/PopUpWrapper";
 import { UserRolesData } from "../../staticData/roles";
 import { PasswordInput } from "../ui/form/PasswordInput";
+import * as yup from 'yup'
+import { Formik } from "formik";
 interface UserCreatePopupInterface {
     popupState:boolean,
     closePopup:()=>void
 }
 export const UserCreatePopup:FC<UserCreatePopupInterface> =({popupState,closePopup})=>{
+    const validationSchema = yup.object().shape({
+        name:yup.string().typeError('Должно быть строкой').required('Обязательное поле').length(3, '3 знака минимум')
+    })
+
     return(
         <PopUpWrapper 
         state={popupState} 
         close={closePopup} >
             <div className="flex flex-col gap-5">
                 <h1 className=" font-bold text-2xl">Создание пользователя</h1>
-                <form className="flex flex-col gap-3" action="#">
+                <Formik
+                initialValues={{
+                    name:'',
+                    family:'',
+                    email:'',
+                    password:'',
+                    role:''
+                }}
+                validateOnBlur
+                onSubmit={(values)=>console.log(values)}
+                validationSchema={validationSchema}
+                >
+                        {({values,errors, touched, handleChange,handleBlur,isValid,handleSubmit,dirty})=>(
+                       <form className="flex flex-col gap-3" >
+                            <InputField 
+                                title={"Имя"} 
+                                name={"name"} 
+                                placeholder={"UserName"} 
+                                type="text"
+                                onblure={handleBlur}
+                                onchange={handleChange}
+                                value={values.name}
+                                touched={touched.name}
+                                error={errors.name}
+
+                            />
+                            <ButtonType1
+                                name="Создать пользователя" 
+                                type="submit"
+                            />
+                            
+                        </form>
+
+                        )}
+                </Formik>
+                {/* <form className="flex flex-col gap-3" action="#">
                     <InputField 
                         title={"Имя"} 
                         name={"name"} 
@@ -44,7 +85,7 @@ export const UserCreatePopup:FC<UserCreatePopupInterface> =({popupState,closePop
                     <ButtonType1
                     name="Создать пользователя" type="submit"/>
             
-                </form>
+                </form> */}
             </div>
 
             
