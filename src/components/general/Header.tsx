5a878cbfb62c34a1176f970a922b0c7f4e7c8304
FAST <krinickij.v.a@gmail.com>
2024-11-06 19:26:16 +0300

    feat: aded logout function to header button
    Добавлена функция выхода в шапке страницы

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom"
+import { useAppDispatch } from "../../hooks/redux"
+import { authSlice } from "../../store/slices/authSlice"
 import { WhiteButton } from "../ui/buttons/WhiteButton"
 import { UserRole } from "../ui/UserRole"
+import { Routes } from "../../navigation/routes"
 
 export const Header = () => {
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  const exit =()=>{
+    dispatch(authSlice.actions.authLogOut())
+    navigate(Routes.login)
+  }
+
   return (
     <nav className="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
       <div className="flex flex-wrap justify-between items-center">
@@ -19,6 +31,7 @@ export const Header = () => {
             <WhiteButton
               title={"LogOut"}
               icon='exit'
+              onclick={()=>exit()}
             />
           </div>
 
