578b7105067fb26647cb6a3e76bd51bea254efbe
FAST <krinickij.v.a@gmail.com>
2024-11-06 12:15:41 +0300

    FEAT: edited  login form
    Доработана форма входа
    добавлена валидация для формы входа
    в env добавлены поля  логина и пароля для простоты входа
    Доработано поле ввода данных для formik

--- src/components/ui/form/formik/InputField.tsx
+++ src/components/ui/form/formik/InputField.tsx
@@ -1,7 +1,6 @@
 import { Field } from "formik";
 import { FC } from "react";
 interface inputfieldInterfase{
-    validateFunc:()=>void,
     error:string | undefined,
     touched: boolean | undefined,
     type:'email' | 'text' |'password'
@@ -9,25 +8,24 @@ interface inputfieldInterfase{
     placeholder:string,
     title:string
 }
-export const InputField:FC<inputfieldInterfase> =({title,name, error,touched,type, validateFunc,placeholder='placeholder'})=>{
+export const InputField:FC<inputfieldInterfase> =({title,name, error,touched,type,placeholder='placeholder'})=>{
     return(
         <label>
-        <span>{title}</span>
+            <span className={`${error && touched && 'flex justify-between items-center'} ` }>
+            <span>{title}</span>
+            <span className="mt-2 text-sm text-red-600 dark:text-red-500">
+            {error}
+        </span>
+            </span>
+
         <Field
             type={type}
             name={name}
             placeholder={placeholder}
             className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${error && touched && "bg-red-200 border-red-500"
-                }`}
-            validate={validateFunc} />
-        <div className="mt-2 text-sm text-red-600 dark:text-red-500">
-            {error}
-        </div>
-        {error && touched && (
-            <div className="mt-2 text-sm text-red-600 dark:text-red-500">
-                {error}
-            </div>
-        )}
+                }`}/>
+
+
     </label>
     )
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,29 +1,48 @@
 import { FC } from "react";
 import { ButtonType1 } from "../components/ui/buttons/SybmitButton";
-import { Field, Form, Formik } from "formik";
-
-import { validateField } from "../heplers/formValidation";
-import { NavLink } from "react-router-dom";
+import { Form, Formik } from "formik";
+import {  useNavigate } from "react-router-dom";
 import { InputField } from "../components/ui/form/formik/InputField";
 import { Routes } from "../navigation/routes";
+import * as yup from 'yup';
+import { sha512 } from "js-sha512";
 
 export const LoginPage: FC = () => {
+    let login = import.meta.env.VITE_ROOT_ADMIN_LOGIN
+    let password =import.meta.env.VITE_ROOTD_AMIN_PASSWORD
+    if(!password || !login){
+        login =''
+        password=''
+    }
+
+    const validationSchema = yup.object().shape({
+        email:yup.string().email('Некорректный email').required('Обязательное поле'),
+        password:yup.string().required('Обязательное поле')
+    })
+    const navigate = useNavigate()
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <NavLink to={Routes.home}> Логин мимо админки</NavLink>
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
-                            Slam`s Adminка
+                        <h1 className=" text-center mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 ">
+                        Bim-Bim admin 
                         </h1>
                         <Formik
                             initialValues={{
-                                email: "",
-                                password: ""
+                                email: `${login ? login : ''}` ,
+                                password: `${password ? password : ''}` 
                             }}
-                            onSubmit={(values) => console.log('asdfaa')}
+                            validateOnBlur
+
+                            onSubmit={(values) => {
+                                // values.password = sha512(values.password)
+        
+                                navigate(Routes.home)
+
+                            }}
+                            validationSchema={validationSchema}
                         >
                             {({ errors, touched }) => (
                                 <Form className=" flex flex-col gap-4">
@@ -31,8 +50,7 @@ export const LoginPage: FC = () => {
                                         title='email'
                                         name="email"
                                         type='email'
-                                        placeholder='Почта Слэма'
-                                        validateFunc={() => validateField}
+                                        placeholder='Почта'
                                         error={errors.email}
                                         touched={touched.email} />
                                     <InputField
@@ -40,12 +58,11 @@ export const LoginPage: FC = () => {
                                         name="password"
                                         type='password'
                                         placeholder='Пароль Слэма'
-                                        validateFunc={() => validateField}
                                         error={errors.password}
                                         touched={touched.password} />
                                     <ButtonType1
                                         type="submit"
-                                        name="Молви Слэм и войди!" />
+                                        name="Вход" />
                                 </Form>
                             )}
                         </Formik>
